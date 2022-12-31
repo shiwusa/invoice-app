c8fd0dd7b56ba47dc1fc3cf5667faf289a94c1ef
shiwusa <lekh0t0er@gmail.com>
2022-12-31 01:36:16 +0100

    add authentication with jwt and cookie
    implement login and register functions

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,16 +1,41 @@
-import React from "react";
-import {Link} from 'react-router-dom';
+import React, {useState} from "react";
+import {Link, useNavigate} from 'react-router-dom';
+import axios from "axios";
 
 const Register = () => {
+    const [inputs, setInputs] = useState({
+        username: "",
+        email: "",
+        password: "",
+    });
+
+    const [err, setError] = useState(null);
+
+    const navigate = useNavigate();
+
+    const handleChange = (e) => {
+        setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            await axios.post("/auth/register", inputs);
+            navigate("/login");
+        } catch (err) {
+            setError(err.response.data);
+        }
+    };
+
     return (
         <div className="auth">
             <h1>Register</h1>
             <form >
-                <input required type="text" placeholder="username"/>
-                <input required type="email" placeholder="email"/>
-                <input required type="password" placeholder="password"/>
-                <button>Login</button>
-                <p>For errors</p>
+                <input required type="text" placeholder="username" name="username" onChange={handleChange}/>
+                <input required type="email" placeholder="email" name="email" onChange={handleChange}/>
+                <input required type="password" placeholder="password" name="password" onChange={handleChange}/>
+                <button onClick={handleSubmit}>Register</button>
+                {err && <p>{err}</p>}
                 <span>Don you have an account? <br/>
                     <Link to="/login">Login</Link>
                 </span>
