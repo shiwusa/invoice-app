d6a0e659d3277275146eec81400ebca4360e1ff8
shiwusa <lekh0t0er@gmail.com>
2023-06-15 15:42:54 +0300

    change port

--- server/config.js
+++ server/config.js
@@ -1,14 +1,14 @@
 const config = {
-    port: 8800,
-    hostname: "localhost",
-    db: {
-        host: "localhost",
-        user: "root",
-        password: "",
-        database: "invoice",
-    },
-    staticSalt: "$2a$10$KQJMYD/wOfnSXK8hS.WR5e",
-    jwtSecret: "y9hKr7f2esCwB89FLg3RjM5DtK6SnpXZ"
+  port: process.env.PORT || 8800,
+  hostname: "localhost",
+  db: {
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "invoice",
+  },
+  staticSalt: "$2a$10$KQJMYD/wOfnSXK8hS.WR5e",
+  jwtSecret: "y9hKr7f2esCwB89FLg3RjM5DtK6SnpXZ",
 };
 
 export default config;
